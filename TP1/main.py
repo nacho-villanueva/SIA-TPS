b23be846fc0ab3df872c2f1036400a6305bd5fa5
gbarbieripederiva <gbarbieripederiva@gmail.com>
2021-08-06 01:55:23 -0300

    now playable

--- TP1/main.py
+++ TP1/main.py
@@ -5,50 +5,74 @@ import arcade
 import sys
 from functools import reduce
 
+class SokobanApplication(arcade.Window):
+    def __init__(self,sokoban, window_height = 600, window_width = 600, window_title = "SIA Sokoban", draw_interval = 1, resizable = True):
+        super().__init__(window_width, window_height, window_title,resizable=resizable)
+        
+        self.sokoban = sokoban
+        self.window_height = window_height 
+        self.window_width = window_width 
+        self.window_title = window_title 
+        self.draw_interval = draw_interval 
 
-# Constants
-WINDOW_HEIGHT = 600
-WINDOW_WIDTH = 600
-WINDOW_TITLE = "SIA Sokoban"
-DRAW_INTERVAL = 1
+    def on_draw(self):
+        # Iterate through the states and draw
+        state = self.sokoban.state
+        BLOCK_WIDTH = int (self.window_width / state.dimensions[1])
+        BLOCK_HEIGHT = int (self.window_height / state.dimensions[0])
+        SPACING = 10
+        
+        arcade.start_render()
+        for i in range(state.dimensions[0]):
+            for j in range(state.dimensions[1]):
+                val = ' '
+                if state.dynamic_state[i][j] != GameState.EMPTY:
+                    val = state.dynamic_state[i][j]
+                else:
+                    val = state.static_state[i][j]
 
-def ondraw(dt):
-    # Iterate through the states and draw
-    state = ondraw.sokoban.state
-    BLOCK_WIDTH = int (WINDOW_WIDTH / state.dimensions[1])
-    BLOCK_HEIGHT = int (WINDOW_HEIGHT / state.dimensions[0])
-    SPACING = 10
-    
-    arcade.start_render()
-    for i in range(state.dimensions[0]):
-        for j in range(state.dimensions[1]):
-            val = ' '
-            if state.dynamic_state[i][j] != GameState.EMPTY:
-                val = state.dynamic_state[i][j]
-            else:
-                val = state.static_state[i][j]
+                if val == GameState.BLOCK_WALL:
+                    arcade.draw_rectangle_filled(
+                        BLOCK_WIDTH * j + BLOCK_WIDTH/2,
+                        self.window_height - BLOCK_HEIGHT * i - BLOCK_HEIGHT/2,
+                        BLOCK_WIDTH - SPACING,BLOCK_HEIGHT - SPACING,
+                        arcade.color.WOOD_BROWN
+                        )
+                elif val == GameState.ICE or val == GameState.ICE_ON_END:
+                    arcade.draw_rectangle_filled(
+                        BLOCK_WIDTH * j + BLOCK_WIDTH/2,
+                        self.window_height - BLOCK_HEIGHT * i - BLOCK_HEIGHT/2,
+                        BLOCK_WIDTH - SPACING,BLOCK_HEIGHT - SPACING,
+                        arcade.color.LIGHT_BLUE
+                        )
+                elif val == GameState.PLAYER or val == GameState.PLAYER_ON_END:
+                    arcade.draw_rectangle_filled(
+                        BLOCK_WIDTH * j + BLOCK_WIDTH/2,
+                        self.window_height - BLOCK_HEIGHT * i - BLOCK_HEIGHT/2,
+                        BLOCK_WIDTH - SPACING,BLOCK_HEIGHT - SPACING,
+                        arcade.color.TEA_GREEN
+                        )
+        arcade.draw_rectangle_filled(0,0,10,10,arcade.color.BLUE)
 
-            if val == GameState.BLOCK_WALL:
-                arcade.draw_rectangle_filled(
-                    WINDOW_HEIGHT - BLOCK_WIDTH * j - BLOCK_WIDTH/2,
-                    WINDOW_HEIGHT - BLOCK_HEIGHT * i - BLOCK_HEIGHT/2,
-                    BLOCK_WIDTH - SPACING,BLOCK_HEIGHT - SPACING,
-                    arcade.color.WOOD_BROWN
-                    )
-            elif val == GameState.ICE or val == GameState.ICE_ON_END:
-                arcade.draw_rectangle_filled(
-                    WINDOW_HEIGHT - BLOCK_WIDTH * j - BLOCK_WIDTH/2,
-                    WINDOW_HEIGHT - BLOCK_HEIGHT * i - BLOCK_HEIGHT/2,
-                    BLOCK_WIDTH - SPACING,BLOCK_HEIGHT - SPACING,
-                    arcade.color.LIGHT_BLUE
-                    )
-            elif val == GameState.PLAYER or val == GameState.PLAYER_ON_END:
-                arcade.draw_rectangle_filled(
-                    WINDOW_HEIGHT - BLOCK_WIDTH * j - BLOCK_WIDTH/2,
-                    WINDOW_HEIGHT - BLOCK_HEIGHT * i - BLOCK_HEIGHT/2,
-                    BLOCK_WIDTH - SPACING,BLOCK_HEIGHT - SPACING,
-                    arcade.color.TEA_GREEN
-                    )
+
+    def on_resize(self, width: float, height: float):
+        self.window_width = width
+        self.window_height = height
+        return super().on_resize(width, height)
+
+    def on_key_press(self, key, modifiers):
+        if key == arcade.key.UP or key == arcade.key.W:
+            self.sokoban.move(Movement.UP)
+        elif key == arcade.key.DOWN or key == arcade.key.S:
+            self.sokoban.move(Movement.DOWN)
+        elif key == arcade.key.LEFT or key == arcade.key.A:
+            self.sokoban.move(Movement.LEFT)
+        elif key == arcade.key.RIGHT or key == arcade.key.D:
+            self.sokoban.move(Movement.RIGHT)
+        elif key == arcade.key.Q:
+            self.close()
+        
+
     
     
 
@@ -85,10 +109,8 @@ def get_code(config_path):
 def main(initial_state):
     state = GameState.from_code(get_code(initial_state))
     sokoban = Sokoban(state)
-    ondraw.sokoban = sokoban
-    # Open window, set drawing function and start running
-    arcade.open_window(WINDOW_WIDTH,WINDOW_HEIGHT,WINDOW_TITLE)
-    arcade.schedule(ondraw,DRAW_INTERVAL)
+    
+    sokoban_app = SokobanApplication(sokoban=sokoban)
     arcade.run()
 
 if __name__ == "__main__":
