adde9502052250ed3428a2147ff920e9708812e8
gbarbieripederiva <gbarbieripederiva@gmail.com>
2021-09-10 18:16:34 -0300

    creo que esta el lineal pero tengo que revisar mejor todo
    tambien hice varias tareas de refactoreo, tengo
    que reveer la clase para checkear que no haya hecho cualquiera

--- TP3/constants.py
+++ TP3/constants.py
@@ -1,5 +1,16 @@
-from TP3.perceptronSimpleEscalon import PerceptronSimpleEscalon
+from TP3.perceptronSimple import PerceptronSimple
 
 def get_algorithm(algorithm):
-    if algorithm == "perceptron_simple":
-        return PerceptronSimpleEscalon.run
+    if algorithm == "perceptron_simple_escalon":
+        perceptron = PerceptronSimple()
+        def escalon(h,w0):
+            if h > w0:
+                return 1
+            else:
+                return -1
+        perceptron.act_fun = escalon
+        return perceptron.run
+    if algorithm == "perceptron_simple_lineal":
+        perceptron = PerceptronSimple()
+        perceptron.act_fun = lambda h,w0: h
+        return perceptron.run
--- TP3/perceptronSimple.py
+++ TP3/perceptronSimple.py
@@ -2,16 +2,15 @@ import numpy as np
 import pandas as pd
 import random
 
-class PerceptronSimpleEscalon():
-    def __init__(self,w=None,learning_rate=0.5,w0=0) -> None:
+class PerceptronSimple():
+    def __init__(self,w=None,act_func=lambda h,w0:h,learning_rate=0.5,w0=0) -> None:
         self.w = np.zeros(2) if w is None else w
         self.w0 = w0
         self.learning_rate = learning_rate
+        self.act_fun = act_func
     def fire(self, xi):
-        if np.sum(self.w * xi) > self.w0:
-            return 1
-        else:
-            return -1
+        return self.act_fun(np.sum(self.w * xi),self.w0)
+
     def update(self,xi,zeta,O):
         self.w = self.w + self.learning_rate*(zeta-O)*xi
 
@@ -22,24 +21,22 @@ class PerceptronSimpleEscalon():
         error = 0
         for i in range(len(x)):
             O = self.fire(x.iloc[i])
-            error += abs(y.iloc[i] - O)
-        return error
+            error += (y.iloc[i] - O) ** 2
+        return error / 2
 
-
-    @staticmethod
-    def run(x:pd.DataFrame,y:pd.DataFrame,COTA=100):
+    def run(self,x:pd.DataFrame,y:pd.DataFrame,COTA=100):
         i = 0
         p = x.shape[0]
-        perceptron = PerceptronSimpleEscalon(np.zeros(len(x.columns)))
+        self.w = np.zeros(len(x.columns))
         error = 1
         errormin = p * 2
         while error > 0 and i < COTA:
             ix = random.randint(0,p-1)
-            O = perceptron.fire(x.iloc[ix])
-            perceptron.update(x.iloc[ix],y.iloc[ix],O)
-            error = perceptron.calculate_error(x, y)
+            O = self.fire(x.iloc[ix])
+            self.update(x.iloc[ix],y.iloc[ix],O)
+            error = self.calculate_error(x, y)
             if error < errormin:
                 errormin = error
-                wmin = perceptron.w
+                wmin = self.w
             i = i + 1
-        return perceptron
+        return self
