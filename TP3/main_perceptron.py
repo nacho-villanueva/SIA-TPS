c39f9d169f87d6674f9ae81a6bfafa5fe0c7153a
Ignacio Villanueva <ivillanueva@itba.edu.ar>
2021-09-25 12:56:00 -0300

    Clean up

--- TP3/main_perceptron.py
+++ TP3/main_perceptron.py
@@ -1,42 +1,17 @@
-import numpy as np
 import pandas as pd
 from matplotlib import pyplot as plt
 
 from TP3.function import Function
+from TP3.methods import *
 from TP3.perceptron import Perceptron
 
-
-def tanh(x):
-    return np.tanh(x)
-
-
-def d_tanh(x):
-    return 1 - tanh(x) ** 2
-
-
-def sigmoid(x):
-    return 1 / (1 + np.exp(-x))
-
-
-def d_sigmoid(x):
-    return sigmoid(x) * (1 - sigmoid(x))
-
-
-def error(expected, actual):
-    return 0.5 * (expected - actual) ** 2
-
-
-def d_error(expected, actual):
-    return expected - actual
-
-
-layers = [2, 3, 2, 1]
+layers = [2, 1]
 training_set = pd.read_csv("data/perceptronSimpleY.csv", sep=";")
 
 X = training_set.drop("y", axis=1).to_numpy().reshape(layers[0], -1)
 Y = training_set.loc[:, "y"].to_numpy().reshape(layers[-1], -1)
 
-nn = Perceptron(layers, Function(tanh, d_tanh), Function(error, d_error))
+nn = Perceptron(layers, Function(stair(), d_stair), Function(error, d_error))
 
 nn.train(X, Y, epochs=10000, batch_size=4, learning_rate=.01)
 prediction = nn.feedforward(X)
--- TP3/methods.py
+++ TP3/methods.py
@@ -1,6 +1,47 @@
-def mse(expected: float, actual: float):
-    return 0.5 * ((actual - expected) ** 2)
+import numpy as np
 
 
-def d_mse(expected: float, actual: float):
-    return actual - expected
+def tanh(x):
+    return np.tanh(x)
+
+
+def d_tanh(x):
+    return 1 - tanh(x) ** 2
+
+
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+
+def d_sigmoid(x):
+    return sigmoid(x) * (1 - sigmoid(x))
+
+
+def stair():
+    def _stair(x):
+        if x < 0:
+            return -1
+        else:
+            return 1
+
+    return np.vectorize(_stair)
+
+
+def d_stair(x):
+    return 0
+
+
+def lineal(x):
+    return x
+
+
+def d_lineal(x):
+    return 1
+
+
+def error(expected, actual):
+    return 0.5 * (expected - actual) ** 2
+
+
+def d_error(expected, actual):
+    return expected - actual
